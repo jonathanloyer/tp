01446112d3f82c809d7f7c476d0515fa380bd33e
jochouca <jonathanloyer1@gmail.com>
2024-10-23 13:51:18 +0200

    forcorrection

--- src/Controller/TodosController.php
+++ src/Controller/TodosController.php
@@ -4,20 +4,27 @@ namespace App\Controller;
 
 use App\Entity\Taches;
 use App\Repository\MessageRepository;
+use App\Repository\TachesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodosController extends AbstractController
 {
     #[Route("/todos", name: "todo", methods: ["GET"])]
-    function contact()
+    function contact(TachesRepository $repo)
     {
-        return $this->render('todos.html.twig');
+        // Récupérer toutes les tâches depuis la base de données
+        $taches = $repo->findAll();
+    
+        // Rendre la vue avec les tâches
+        return $this->render('todos.html.twig', [
+            'taches' => $taches  // Passer les tâches à la vue
+        ]);
     }
-
     #[Route("/todos/traitement", name: "todos.traitement", methods: ["POST"])]
-    function addList(Request $req, MessageRepository $repo)
+    function addList(Request $req, TachesRepository $repo)
     {
         $nom = $req->request->get('nom');
         // dump($req->request);
@@ -32,7 +39,17 @@ class TodosController extends AbstractController
 
         $repo->save($newTaches,true);
 
-        return $this->render('todos.html.twig',['sucess' =>true, 'nom' => "donnée envoyé"]
+        $taches = $repo->findAll();
+
+        return $this->render('todos.html.twig',['success' =>true, 'nom' => "donnée envoyé", 'taches'=>$taches]
     );
     }
+    // src/Controller/TacheController.php
+
+
+
+    
 }
+
+
+
